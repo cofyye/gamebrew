50d9f71d20fd6d251a9b48665995ee5f38bd64f7
cofyye <cofyye@gmail.com>
2025-09-28 21:30:59 +0200

    feat: added test footer component to test GitHub CI/CD Workflows

---
+++ frontend/components/shared/footer.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export function Footer() {
+  return (
+    <footer className="flex flex-wrap items-center justify-center gap-4 py-8 text-sm text-gray-500 dark:text-gray-400">
+      <a
+        className="hover:underline hover:underline-offset-4"
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer">
+        Powered by{" "}
+        <span className="font-semibold">Vercel</span>
+      </a>
+      </footer>
+  )
+}
